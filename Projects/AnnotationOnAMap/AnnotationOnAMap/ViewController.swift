db86442b6ca1ea8f98928524481a6caee4e4bc87
John <ionut_popovici92@yahoo.com>
2017-11-21 12:25:58 +0200

    Added AnnotationOnAMap project - final

--- Projects/AnnotationOnAMap/AnnotationOnAMap/ViewController.swift
+++ Projects/AnnotationOnAMap/AnnotationOnAMap/ViewController.swift
@@ -9,18 +9,20 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, MKMapViewDelegate {
     
     @IBOutlet weak var map: MKMapView!
-    
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
         
         let span: MKCoordinateSpan = MKCoordinateSpanMake(0.1, 0.1)
         let location: CLLocationCoordinate2D = CLLocationCoordinate2DMake(64.126431, -21.816881)
         let region: MKCoordinateRegion = MKCoordinateRegionMake(location, span)
         
         map.setRegion(region, animated: true)
+        // map.delegate = self
+        // print(self)
         
         let annotation = MKPointAnnotation()
         annotation.coordinate = location
@@ -30,14 +32,13 @@ class ViewController: UIViewController {
         map.addAnnotation(annotation)
     }
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-    }
-
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    public func mapView(_ mapView: MKMapView, regionWillChangeAnimated animated: Bool) {
+        
     }
 
 }
